197834a5b3c86002d49557f2aa7d121cbd90f53c
Colin Gladden <crg9332@g.rit.edu>
2022-02-01 12:19:49 -0500

    Removed useless comments

--- brutehash.py
+++ brutehash.py
@@ -6,18 +6,11 @@ from time import time
 class word():
     def re():
         str = input("Enter an MD5 code to be decoded: ")
-        #result = hashlib.md5(str.encode())
-
-        #print("The hexadecimal equivalent of hash is : ", end ="")
-        #end = result.hexdigest()
         return str
 
 class guess_word():
     def gu(x):
         result = hashlib.md5(x.encode())
-
-        #print("The hexadecimal equivalent of hash is : ", end ="")
-        #gue = result.hexdigest()
         return result.hexdigest()
 
 class comp():
